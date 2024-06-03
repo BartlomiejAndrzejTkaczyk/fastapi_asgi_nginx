200d071dcb8d180482a505d810629a6088fcd1eb
kruk97 <76618368+BartlomiejAndrzejTkaczyk@users.noreply.github.com>
2024-06-04 01:59:35 +0200

    fix loaderio

--- loader_io/router.py
+++ loader_io/router.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.responses import HTMLResponse
 
 router = APIRouter()
 
 
-@router.get('/loaderio-b870ca900bf56f434c0db90bf2f6961a/')
+@router.get('/loaderio-b870ca900bf56f434c0db90bf2f6961a/', response_class=HTMLResponse)
 def loader_io():
-    return 'loaderio-b870ca900bf56f434c0db90bf2f6961a'
+    html_content = 'loaderio-b870ca900bf56f434c0db90bf2f6961a'
+    return HTMLResponse(content=html_content, status_code=status.HTTP_200_OK)
 
